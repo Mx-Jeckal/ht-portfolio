4946536240ce8031e6183340c99e0694eaa88da2
Mx-Jeckal <mx.jeckal@gmail.com>
2020-05-07 00:03:34 -0500

    set up app and placed header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css'
+
+
+function Header(){
+    return(
+<div class="header">
+  <div class="sides">
+    <a href="#" class="logo">BLOG</a>
+  </div>
+  <div class="sides"> <a href="#" class="menu"> </a></div>
+    <div class="info">
+        <h4><a href="#category">UI DESIGN</a></h4>
+        <h1>KEN BURNS HEADERS ARE THE BEST</h1>
+        <div class="meta">
+        <a  href="https://twitter.com/nodws" target="_b" class="author"></a><br />
+        By <a href="https://twitter.com/nodws" target="_b">James Nodws</a> on May 30, 2019
+    </div>
+  </div>
+</div>
+
+    )
+}
+
+
+export default Header;
